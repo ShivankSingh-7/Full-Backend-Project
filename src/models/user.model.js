ce491692eefa3a4b8c68ab1dd73e9307929445eb
Shivank Singh <shivangsingh819@gmail.com>
2025-07-28 11:20:46 +0530

    created user controller and also fixes few bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
@@ -51,7 +51,7 @@ const userSchema = new Schema(
 //here we are incripting the password learn about this more
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
